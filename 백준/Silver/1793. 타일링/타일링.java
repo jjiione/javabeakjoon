897d2d08824f4292eb1e6b487db40cbc345fafd1
Ji Won Jeong <97580459+jjiione@users.noreply.github.com>
2023-05-12 11:36:57 +0900

    [Silver II] Title: 타일링, Time: 96 ms, Memory: 12416 KB -BaekjoonHub

--- 백준/Silver/1793. 타일링/타일링.java
+++ 백준/Silver/1793. 타일링/타일링.java
@@ -1,27 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.math.BigInteger;
-import java.util.Scanner;
 
 public class Main {
-    static BigInteger[] memoArray = null;
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        memoArray = new BigInteger[251];
-        memoArray[0] = BigInteger.valueOf(1);
-        memoArray[1] = BigInteger.valueOf(1);
-        while(scan.hasNextInt()) {
-            int inputData = Integer.parseInt(scan.nextLine().trim());
-            
-            for(int i=0; i <= inputData; i++) {
-                if(memoArray[i] != null) {
-                    continue ;
-                }
-                
-                BigInteger mul = memoArray[i-2].multiply(BigInteger.valueOf(2));
-                BigInteger data = memoArray[i-1];
-                memoArray[i] = data.add(mul);
-            }
-            
-            System.out.println(memoArray[inputData]);
-        }
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+		BufferedWriter sw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BigInteger[] dp = new BigInteger [251];
+		dp[0] = new BigInteger("1");
+		dp[1] = new BigInteger("1");
+		dp[2] = new BigInteger("3");
+		for(int i=3; i<251; i++) {
+			BigInteger tmp =dp[i-2].multiply(new BigInteger("2"));
+			tmp = tmp.add(dp[i-1]);
+			
+			dp[i] = tmp;
+		}
+
+		String str = null;
+		while((str = br.readLine()) != null) {
+//			System.out.println(dp[Integer.parseInt(str)]);
+			sw.write(dp[Integer.parseInt(str)].toString());
+			sw.newLine();
+		}
+		sw.flush();
+	}
+
 }
