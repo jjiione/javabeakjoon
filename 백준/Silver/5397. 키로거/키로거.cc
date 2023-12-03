c074ca0109eac6b91742762a7ce9a9b27dd65ea0
Ji Won Jeong <97580459+jjiione@users.noreply.github.com>
2023-12-03 23:45:17 +0900

    [Silver II] Title: 키로거, Time: 116 ms, Memory: 5708 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while(T--) {
+        stack<char> s1;
+        stack<char> s2;
+        string L;
+        cin >> L;
+        for (char letter : L) {
+            if (letter == '<') {
+                if (s1.empty()) continue;
+                s2.push(s1.top());
+                s1.pop();
+            } else if (letter == '>') {
+                if (s2.empty()) continue;
+                s1.push(s2.top());
+                s2.pop();
+            } else if (letter == '-') {
+                if (s1.empty()) continue;
+                s1.pop();
+            } else {
+                s1.push(letter);
+            }
+        }
+
+        while(!s2.empty()) {
+            s1.push(s2.top());
+            s2.pop();
+        }
+
+        while(!s1.empty()) {
+            s2.push(s1.top());
+            s1.pop();
+        }
+
+        while(!s2.empty()) {
+            cout << s2.top();
+            s2.pop();
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}
